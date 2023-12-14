dd70a2102cc5fc4570bbba809a36e57cdacbacda
Hmazaberkawy <abomwafak22222@gmail.com>
2023-12-14 19:04:28 +0300

    Mouhamad_Hamza_Al_Barkawy_&&_Amer_Dawed

--- Binary_Search_Homework/BinarySearchHomework.cs
+++ Binary_Search_Homework/BinarySearchHomework.cs
@@ -13,7 +13,34 @@ namespace Binary_Search_Homework // don't edit this line!!!
                                                                                 // لكن لاتقوم بحذفهم أو التعديل عليهم هنا تحت اي ظرف!!!
                                                                                 // من الممكن تمرير قيمة 0 لتجاهلهم
         {
-            //TODO: write code for the ternary search algorithm and return the index of the element
+            if (start <= end)
+            {
+                int partition_size = (end - start) / 3;
+                int right_mid = start + partition_size;
+                int left_mid = end - partition_size;
+
+                if (arr[right_mid] == key)
+                {
+                    return right_mid;
+                }
+
+                if (arr[left_mid] == key)
+                {
+                    return left_mid;
+                }
+                if (key < arr[right_mid])
+                {
+                    return TernarySearch(arr, key, start, right_mid - 1);
+                }
+                if (key > arr[left_mid])
+                {
+                    return TernarySearch(arr, key, left_mid + 1, end);
+                }
+                else
+                {
+                    return TernarySearch(arr, key, right_mid + 1, left_mid - 1);
+                }
+            }
             return -1;
         }
 
@@ -23,15 +50,53 @@ namespace Binary_Search_Homework // don't edit this line!!!
                                                                     // لكن لاتقوم بحذفهم أو التعديل عليهم هنا تحت اي ظرف!!!
                                                                     // من الممكن تمرير قيمة 0 لتجاهلهم
         {
-            //TODO: this methods is for getting the first accurence of the key and the last accurance
+            if (end >= start)
+            {
+                int mid = start + (end - start) / 2;
+                if (arr[mid] == key)
+                {
+                    if (is_first)
+                    {
+                        if (mid == start || arr[mid - 1] != key)
+                        {
+                            return mid;
+                        }
+                        else
+                        {
+                            return BinarySearchForCalculatingRepeated(arr, key, is_first, start, mid - 1);
+                        }
+                    }
+
+                    else
+                    {
+                        if (mid == end || arr[mid + 1] != key)
+                        {
+                            return mid;
+                        }
+                        else
+                        {
+                            return BinarySearchForCalculatingRepeated(arr, key, is_first, mid + 1, end);
+                        }
+                    }
+                }
+                if (arr[mid] > key)
+                {
+                    return BinarySearchForCalculatingRepeated(arr, key, is_first, start, mid - 1);
+                }
+                return BinarySearchForCalculatingRepeated(arr, key, is_first, mid + 1, end);
+            }
+
             return -1;
         }
 
         public static int GetRepeatCount(int[] arr, int key) // don't edit this line!!!
         {
-            //TODO: write code to calculate the repeat count of a spacific element
-            // make sure to use the previous method in this method
-            return -1;
+            int firstElement = BinarySearchForCalculatingRepeated(arr, key, true, 0, arr.Length - 1);
+            int lastElement = BinarySearchForCalculatingRepeated(arr, key, false, 0, arr.Length - 1);
+
+            return lastElement - firstElement + 1;
+
         }
     }
 }
+
